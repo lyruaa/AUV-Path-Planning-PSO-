64c134eacc77e31d169a3631d64366b49f2692e0
lyruaa <15502150450@163.com>
2026-09-19 21:33:00 +0800

    feat: 收敛曲线与能耗曲线对比脚本，量化改进算法优势

---
+++ comp_cost.m
@@ -0,0 +1,25 @@
+﻿%best_cost±???
+Cost1=load('best_cost1.mat','BestCost');
+Cost_comp_1=Cost1.BestCost;
+
+Cost2=load('best_cost11.mat','BestCost');
+Cost_comp_2=Cost2.BestCost;
+
+
+plot(Cost_comp_1,'-k','LineWidth',2);
+hold on;
+plot(Cost_comp_2,'-r','LineWidth',1);
+hold on;
+legend('DENPSO','LPSO')
+xlabel('Iteration');
+ylabel('Cost value');
+hold on
+box off
+ax2 = axes('Position',get(gca,'Position'),...
+           'XAxisLocation','top',...
+           'YAxisLocation','right',...
+           'Color','none',...
+           'XColor','k','YColor','k');
+set(ax2,'YTick', []);
+set(ax2,'XTick', []);
+box on
---
+++ comp_energy.m
@@ -0,0 +1,25 @@
+﻿%能量消耗比较
+Energy1=load('energy1.mat','energy_opt');
+Energy_comp_1=Energy1.energy_opt;
+
+Energy2=load('energy11.mat','energy_opt');
+Energy_comp_2=Energy2.energy_opt;
+
+plot(Energy_comp_1,'-r','LineWidth',1);
+hold on;
+plot(Energy_comp_2,'-k','LineWidth',1);
+hold on;
+legend('NPSO','LPSO')
+xlabel('Iteration');
+ylabel('Energy consumption (J)');
+
+box off
+ax2 = axes('Position',get(gca,'Position'),...
+           'XAxisLocation','top',...
+           'YAxisLocation','right',...
+           'Color','none',...
+           'XColor','k','YColor','k');
+set(ax2,'YTick', []);
+set(ax2,'XTick', []);
+box on
+axis equal
