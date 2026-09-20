5dcaae32284f61970fbf54c4d0ead77f71135e8e
lyruaa <15502150450@163.com>
2026-09-20 20:16:00 +0800

    feat: 跳跃次数与约束违反次数统计对比脚本

---
+++ comp_symbol.m
@@ -0,0 +1,22 @@
+﻿Symbol=load('Symbol.mat','Symbol');
+Sym=Symbol.Symbol;
+for i=1:100
+    if Sym(i,1)==15
+        Sym(i,1)=14;
+    end    
+end
+yy=linspace(1,200,200);
+
+scatter(yy,Sym,'+');
+xlabel('Iteration');
+ylabel('Random perturbation');
+hold on
+box off
+ax2 = axes('Position',get(gca,'Position'),...
+           'XAxisLocation','top',...
+           'YAxisLocation','right',...
+           'Color','none',...
+           'XColor','k','YColor','k');
+set(ax2,'YTick', []);
+set(ax2,'XTick', []);
+box on
---
+++ comp_vio.m
@@ -0,0 +1,23 @@
+﻿%best_cost±???
+Violation1=load('violation1.mat','Violation');
+Violation_comp_1=Violation1.Violation;
+
+Violation2=load('violation11.mat','Violation');
+Violation_comp_2=Violation2.Violation;
+
+plot(Violation_comp_1,'-r','LineWidth',1);
+hold on;
+plot(Violation_comp_2,'-k','LineWidth',1);
+hold on;
+xlabel('Iteration');
+ylabel('Micro\_factor');
+hold on
+box off
+ax2 = axes('Position',get(gca,'Position'),...
+           'XAxisLocation','top',...
+           'YAxisLocation','right',...
+           'Color','none',...
+           'XColor','k','YColor','k');
+set(ax2,'YTick', []);
+set(ax2,'XTick', []);
+box on
